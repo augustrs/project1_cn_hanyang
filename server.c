89199cc4be833442d929343953df86e2c87102dd
augustrs <auso0001@stud.kea.dk>
2025-03-18 11:01:12 +0900

    Fixed bind__socket function

--- server.c
+++ server.c
@@ -17,17 +17,25 @@ int main(int argc, char *argv[]) {
 return 0;
 }
 
-int socket() {
+int create_socket() {
     int server_socket = socket(AF_INET, SOCK_STREAM,  0); // Creates socket using ipv4, tcp and default protocol
     if (server_socket == -1) {
         printf("Failed to create socket\n");
         exit(EXIT_FAILURE);
     }
+    return 0;
 }
 
-int bind(int server_socket, int port) {
+int bind_socket(int server_socket, int port) {
 struct sockaddr_in server_address;
 server_address.sin_family = AF_INET; // ipv4
 server_address.sin_addr.s_addr = INADDR_ANY; // listen to any address (all interfaces)
-server_address.sin_port = htons(port); // 
+server_address.sin_port = htons(port); // set port, needs to be changed to be defined by user
+
+// standard library bind function, different from bind_socket function
+if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+    printf("Failed to bind socket\n");
+    exit(EXIT_FAILURE);
+}
+return 0;
 }
